a8793bb8b0a9fb3bfa5fb7871502468c82d2eeae
MichelleNonato93 <michelle.nonato@gmail.com>
2015-10-20 09:36:48 +0800

    validation contact remove validation in user result error

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,11 @@
 class Contact < ActiveRecord::Base
 
+  validates :first_name, :last_name, :email, presence :true
+
+  validates :message,
+            length {
+              minimum: 30,
+              maximum: 50
+            }
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :first_name, :last_name, :email, presence :true
-
-  validates :username,
-            presence :true,
-            uniqueness :true,
-            length: { maximum: 10 }
-
-  validates :password, length { maximum: 15 }
-
 end
