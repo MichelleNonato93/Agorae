562536e88d73487bacb68217de27037012681539
MichelleNonato93 <michelle.nonato@gmail.com>
2015-10-23 10:04:16 +0800

    add some contents in form and controller

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -1,24 +1,29 @@
 class DetailsController < ApplicationController
 
   def index
+    @details = Detail.all
   end
 
   def show
   end
 
   def new
+    @detail = Detail.new
   end
 
   def edit
   end
 
   def create
-  end
-
-  def update
+    @detail = Detail.new(detail_params)
   end
 
   def destroy
+    @detail = Detail.find(detail_params)
+  end
+
+  def detail_params
+    params.require(:detail).permit(:name, :email, :contact_number, :photo, :created_at, :updated_at)
   end
 
 end
